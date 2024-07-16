082e63e2e56dfa171b782537f9bf09323b83d92e
Ehor122 <ipz23-ye.horbach@nubip.edu.ua>
2024-07-16 22:50:18 +0300

    Add files via upload

---
+++ Pract_6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x1, y1, x2, y2;
+    printf("Enter the coordinates of the origin of the vector (x1 y1): ");
+    scanf("%d %d", &x1, &y1);
+    printf("Enter the coordinates of the end of the vector (x2 y2): ");
+    scanf("%d %d", &x2, &y2);
+
+    // Calculating the length of the vector
+    double length = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    // Print the result to six decimal places
+    printf("Vector length: %.6f\n", length);
+
+    return 0;
+}
